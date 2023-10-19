0b4be43a22f31613cab9f87603a13d6bed6ec3a1
Ankit Kumar <ankitkr1009@gmail.com>
2023-10-20 00:19:42 +0530

    nested children

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+/**
+ * 
+ * <div id='parent'>
+ *    <div id='child1'>
+ *      <h1>I am H1 tag</h1>
+ *      <h2>I am H2 tag</h2>
+ *    </div>
+ *    <div id='child2'>
+ *      <h1>I am H1 tag</h1>
+ *      <h2>I am H2 tag</h2>
+ *    </div>
+ * </div>
+ */
+const child1 = React.createElement(
+  "div",
+  {id: 'child1', key: 'c1'},
+  [React.createElement("h1", {key: 'ch1'}, "I am H1 tag"), React.createElement("h2", {key: 'ch2'}, "I am H2 tag")]
+);
 
-const headingReact = React.createElement(
-  "h1",
-  {id: 'heading', key: 'heading'},
-  "Hello World from React"
+const child2 = React.createElement(
+  "div",
+  {id: 'child2', key: 'c2'},
+  [React.createElement("h1", {key: 'c2h1'}, "I am H1 tag"), React.createElement("h2", {key: 'c2h2'}, "I am H2 tag")]
 );
 
 const subHeading = React.createElement(
@@ -11,10 +29,10 @@ const subHeading = React.createElement(
   "This is sub heading"
 )
 
-const headigContainer = React.createElement(
+const headingContainer = React.createElement(
   'div',
-  {id:"headingContainer"},
-  [headingReact, subHeading]
+  {id:"parent"},
+  [child1, child2]
 )
 
 // console.log(headingReact);
@@ -23,4 +41,4 @@ const rootReact = ReactDOM.createRoot(document.getElementById("root"));
 
 console.log(rootReact);
 
-rootReact.render(headigContainer);
+rootReact.render(headingContainer);
