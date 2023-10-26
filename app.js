7f3f9e133534e5a7677ecb96da07af3aea4e5b23
Ankit Kumar <ankitkr1009@gmail.com>
2023-10-26 15:19:41 +0530

    header added

--- app.js
+++ app.js
@@ -1,54 +1,12 @@
-import {createElement} from "react";
 import {createRoot} from "react-dom/client";
+import Header from './components/Header.js'
 
-/**
- * 
- * <div id='parent'>
- *    <div id='child1'>
- *      <h1>I am H1 tag</h1>
- *      <h2>I am H2 tag</h2>
- *    </div>
- *    <div id='child2'>
- *      <h1>I am H1 tag</h1>
- *      <h2>I am H2 tag</h2>
- *    </div>
- * </div>
- */
-const child1 = createElement(
-  "div",
-  {id: 'child1', key: 'c1'},
-  [createElement("h1", {key: 'ch1'}, "I am H1 tag"), createElement("h2", {key: 'ch2'}, "I am H2 tag")]
-);
-
-const child2 = createElement(
-  "div",
-  {id: 'child2', key: 'c2'},
-  [createElement("h1", {key: 'c2h1'}, "I am H1 tag"), createElement("h2", {key: 'c2h2'}, "I am H2 tag")]
-);
-
-const subHeading = createElement(
-  "h3",
-  {className: 'subheading', key: 'subHeading'},
-  "This is sub heading"
+const App = () => (
+  <div className="app">
+    <Header/>
+  </div>
 )
-
-const headingContainer = createElement(
-  'div',
-  {id:"parent"},
-  [child1, child2]
-)
-
-const Title =  () => (<h1>This is title component</h1>);
-
-const FunctionComponent = () => (
-  <div>
-    <Title/>
-    {child1}
-    This is functional Component</div>
-)
-
-console.log(Title)
 
 const rootReact = createRoot(document.getElementById("root"));
 
-rootReact.render(<FunctionComponent/>);
+rootReact.render(<App/>);
---
+++ components/Header.js
@@ -0,0 +1,25 @@
+function Header() {
+  return (
+    <div>
+      <div className="header">
+        <div className="logo-container">
+          <img
+            className="logo"
+            src="https://logo.com/image-cdn/images/kts928pd/production/bf0bee1632f59363afb4be74978bb5d5bd7293ea-404x397.png"
+            alt="logo"
+          />
+        </div>
+        <div className="nav-container">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
