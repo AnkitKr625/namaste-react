7e9a3a0526e4b64a77cdfebea9deec7938fabdb1
Ankit Kumar <ankitkr1009@gmail.com>
2023-10-28 23:51:54 +0530

    restaurant card

--- components/Body.js
+++ components/Body.js
@@ -1,23 +1,14 @@
 import RestaurantCard from "./RestaurantCard";
+import { restaurantData } from "../data/restaurant";
 
 function Body() {
   return (
     <div className="body">
       <div className="search-container">Search</div>
       <div className="restaurant-container">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        {restaurantData.map((resData) => (
+          <RestaurantCard key={resData.info.id} info={resData.info} />
+        ))}
       </div>
     </div>
   );
--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,17 +1,23 @@
-function RestaurantCard() {
+import Rating from "../assets/Rating.js";
+
+function RestaurantCard({ info }) {
+  const { cloudinaryImageId: imageId, name, cuisines } = info;
   return (
     <div className="restaurant-card">
       <div className="restaurant-img-container">
         <img
           className="restaurant-img"
-          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/iivuhjc2mswi9lublktf"
+          src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${imageId}`}
           alt="res-logo"
         />
       </div>
       <div className="restaurant-detail">
-        <h3 className="res-name">Meghna Foods</h3>
-        <h4>Biryani, North Indian</h4>
-        <h5>4.4 *</h5>
+        <h3 className="res-name">{name}</h3>
+        <p className="res-cuisines">{cuisines.join(",")}</p>
+        <h5>
+          <Rating />
+          4.4
+        </h5>
         <h5>38 min</h5>
       </div>
     </div>
