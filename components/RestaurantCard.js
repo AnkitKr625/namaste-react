00766bf395637292f4b2b9002845b3e47525f3b4
Ankit Kumar <ankitkr1009@gmail.com>
2023-10-29 00:01:29 +0530

    res card design updated

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,7 +1,14 @@
 import Rating from "../assets/Rating.js";
 
 function RestaurantCard({ info }) {
-  const { cloudinaryImageId: imageId, name, cuisines } = info;
+  const {
+    cloudinaryImageId: imageId,
+    name,
+    cuisines,
+    avgRating,
+    sla,
+    areaName,
+  } = info;
   return (
     <div className="restaurant-card">
       <div className="restaurant-img-container">
@@ -13,12 +20,13 @@ function RestaurantCard({ info }) {
       </div>
       <div className="restaurant-detail">
         <h3 className="res-name">{name}</h3>
-        <p className="res-cuisines">{cuisines.join(",")}</p>
-        <h5>
+        <div className="res-rating">
           <Rating />
-          4.4
-        </h5>
-        <h5>38 min</h5>
+          {avgRating}
+          {` . ${sla.deliveryTime} min`}
+        </div>
+        <p className="res-cuisines">{cuisines.join(",")}</p>
+        <div>{areaName}</div>
       </div>
     </div>
   );
