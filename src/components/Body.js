bc9f6350c3a21e3932c13aaea8d37b46477202ec
Ankit Kumar <ankitkr1009@gmail.com>
2023-10-30 20:00:28 +0530

    state in react

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { restaurantData } from "../data/restaurant";
 
 function Body() {
+  const [resList, setResList] = useState(restaurantData);
+
+  function filterResList() {
+    const filteredRes = resList.filter((res) => res.info.avgRating > 4);
+    setResList(filteredRes);
+  }
   return (
     <div className="body">
-      <div className="search-container">Search</div>
+      <div className="search-container">
+        <button onClick={filterResList} className="btn top-rated-btn">
+          Top Rated Restaurant
+        </button>
+        <button
+          onClick={() => setResList(restaurantData)}
+          className="btn reset-btn"
+        >
+          Reset
+        </button>
+      </div>
       <div className="restaurant-container">
-        {restaurantData.map((resData) => (
+        {resList.map((resData) => (
           <RestaurantCard key={resData.info.id} info={resData.info} />
         ))}
       </div>
