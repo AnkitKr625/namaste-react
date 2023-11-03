0b39ffb5f2a35185e5492bbe4b19566cf562d77a
Ankit Kumar <ankitkr1009@gmail.com>
2023-11-03 08:38:21 +0530

    login btn added

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -6,3 +6,8 @@ export const LOGO_URL =
 
 export const API_URL =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const LOGIN_BTN_TEXT = {
+  LOGIN: "Login",
+  LOGOUT: "Logout",
+};
