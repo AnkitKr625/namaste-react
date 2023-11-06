4668b0495340c791d3e16ea4056ddf4ebc94faa9
Ankit Kumar <ankitkr1009@gmail.com>
2023-11-06 23:46:42 +0530

    routes added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import { createRoot } from "react-dom/client";
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
+import About from "./components/About.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Body/>
+  },
+  {
+    path: '/about',
+    element: <About/>
+  }
+])
 
 const App = () => (
   <div className="app">
     <Header />
-    <Body />
+    <RouterProvider router={router}/>
   </div>
 );
 
